f95471b3bcbd253e974e8c1a1083899d45336dce
Zach Chaney <zachary.chaney@gmail.com>
2015-06-08 21:45:13 -0500

    added the necessary updates to main.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,36 @@
-var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
-});
+$(document).ready(function() {
+	var user = new UserModel();
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'edit': 'edit'
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+		}
+	});
 
-var app = new App();
-Backbone.history.start();
+	var app = new App();
+	Backbone.history.start();
+
+	$('#edit').on('submit', function(e) {
+		e.preventDefault();
+		user.set({
+			name: $('#name').val(),
+			role: $('#role').val()
+		});
+	});
+
+	user.on('change', updateProf);
+
+	function updateProf(updateProfModel) {
+		$('.profile-usertitle-name').html(updateProfModel.set('name'));
+		$('.profile-usertitle-job').html(updateProfModel.set('role'));
+	}
+
+});
